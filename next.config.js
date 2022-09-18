83610c2c74571ff996a1ac58f3d518003ab8f84f
changhoe kim <dudnqnfqlc@naver.com>
2022-09-18 21:11:42 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withImages = require('next-images');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    disableStaticImages: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = withImages({
+  ...nextConfig,
+  webpack(config) {
+    return config;
+  },
+});
