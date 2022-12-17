5b7d54d714316c0a6858cef87e419b9e854bd7cb
changhoe kim <dudnqnfqlc@naver.com>
2022-12-17 16:04:03 +0900

    refactor: 수정 작업 진행

--- src/features/blog/components/detail/_stories/BlogPostCommentReply.stories.tsx
+++ src/features/blog/components/detail/_stories/BlogPostCommentReply.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps } from 'react';
 import { ComponentStory, ComponentMeta, ArgTypes } from '@storybook/react';
 import { Box } from '@mui/material';
+import { BlogPostCommentReplySearchModel } from '@features/blog/models';
 import { BlogPostCommentReply } from '../BlogPostCommentReply';
 
 type MyArgTypes = Partial<
@@ -14,17 +15,18 @@ export default {
   argTypes,
 } as ComponentMeta<typeof BlogPostCommentReply>;
 
-function createReplies(length: number, isUser: boolean) {
-  return Array.from({ length }, (_, i) => ({
-    id: String(i),
+function createReplies(isUser = true): BlogPostCommentReplySearchModel {
+  return {
+    id: '1',
     isAnonymous: !isUser,
     createdAt: '2022-04-12',
-    userId: isUser ? String(i) : null,
-    username: isUser ? `유저 ${i}` : `익명 ${i}`,
+    userId: isUser ? '1' : '',
+    username: isUser ? '유저' : '익명',
     value:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit cupiditate facere recusandae! Minima, dicta labore possimus soluta fugiat, aliquid maxime officiis dolorum autem suscipit expedita necessitatibus vitae? Minima, temporibus repellendus!',
-    deletedAt: null,
-  }));
+    deletedAt: '',
+    password: '',
+  };
 }
 
 const Template: ComponentStory<typeof BlogPostCommentReply> = ({
@@ -35,27 +37,25 @@ const Template: ComponentStory<typeof BlogPostCommentReply> = ({
   </Box>
 );
 
-export const ManyReplies = Template.bind({});
-ManyReplies.args = {
-  isOpen: true,
-  replies: createReplies(50, true),
+export const Default = Template.bind({});
+Default.args = {
+  reply: createReplies(),
 };
 
-export const UserReplies = Template.bind({});
-UserReplies.args = {
-  ...ManyReplies.args,
-  replies: createReplies(5, true),
+export const EditMode = Template.bind({});
+EditMode.args = {
+  ...Default.args,
+  userId: '1',
+  replyEditMode: true,
 };
 
-export const AnonymousReplies = Template.bind({});
-AnonymousReplies.args = {
-  ...ManyReplies.args,
-  replies: createReplies(50, false),
+export const Anonymous = Template.bind({});
+Anonymous.args = {
+  reply: createReplies(false),
 };
 
-export const ReplyAuthor = Template.bind({});
-ReplyAuthor.args = {
-  ...ManyReplies.args,
-  userId: '0',
-  replies: createReplies(1, true),
+export const AnonymousEditMode = Template.bind({});
+AnonymousEditMode.args = {
+  ...Anonymous.args,
+  replyEditMode: true,
 };
