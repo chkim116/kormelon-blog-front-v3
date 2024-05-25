30ee0dcb4514c3a9874278396af9e14997f689b3
changhoe kim <kormelon@gmail.com>
2024-05-25 19:35:53 +0900

    feat: bio description 업데이트

--- src/shared/constants/user.const.ts
+++ src/shared/constants/user.const.ts
@@ -1,5 +1,5 @@
 export const USER_NAME = '김창회';
 export const USER_DESCRIPTION =
-  '유지보수 좋은 소프트웨어 개발에 관심이 많은 프론트엔드 개발자, 지금 레벨13에서 일하고 있습니다.';
+  '유지보수 좋은 소프트웨어 개발에 관심이 많은 프론트엔드 개발자, 지금 비바리퍼블리카(토스)에서 일하고 있습니다.';
 
 export const USER_PROFILE_IMAGE_TAG_NAME = 'userProfile';
